571bff91b5d28a15a22c4a290187474ca3e6bb14
Erik <espitzner@pavlovmedia.com>
2022-09-19 13:45:36 -0500

    more

--- alien_invastion.py
+++ alien_invastion.py
@@ -1,15 +1,9 @@
-import pygame, sys, os
+import pygame
 from pygame.sprite import Group
-
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
-from alien import Alien
 import game_functions as gf
-
-if sys.argv:
-    filepath = sys.argv[0]
-    folder, filename = os.path.split(filepath)
-    os.chdir(folder)
 
 
 def run_game():
@@ -19,18 +13,18 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+    stats = GameStats(ai_settings)
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
-    alien = Alien(ai_settings, screen)
     gf.create_fleet(ai_settings, screen, ship, aliens)
-
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
         
--- ship.py
+++ ship.py
@@ -1,22 +1,22 @@
 import pygame
 
-class Ship():
+
+class Ship:
 
     def __init__(self, ai_settings, screen):
         self.screen = screen
         self.ai_settings = ai_settings
-
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
-
         self.center = float(self.rect.centerx)
-
         self.moving_right = False
-        self.moving_left = False        
+        self.moving_left = False
+
+    def center_ship(self):
+        self.cneter = self.screen_rect.centerx
 
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
